171963487255292b91042f088a00dd798bc40b1f
Daniels Ivaskevics <wavesaftwaves@gmail.com>
2023-05-01 13:38:16 +0200

    hotfix finding methods and renew info

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from service.client import Service
 
 
 def main():
+    # Инициализируем сервис, в итоге получим свежую информацию с сайта
     service = Service()
-    # service.read_from_file('db.json')
-    service.update_from_site()
-    service.write_to_file('db.json')
-    if __name__ == "__main__":
-        main()
+    # Запишем всю информацию в файл JSON
+    service.write_to_file()
+    # Ищем вакансии по профессии
+    user_input = input("Какую IT профессию вы хотите найти? ")
+    service.find_by_name(user_input)
+    # Ищем вакансии по зарплате
+    user_input = int(input("Какая зарплата вас бы устроила? "))
+    service.find_by_salary(user_input)
+
+
+if __name__ == "__main__":
+    main()
--- service/client.py
+++ service/client.py
@@ -10,28 +10,35 @@ class Service:
     list_sj: list[SuperJobVacancy] = []
     list_hh: list[HeadHunterVacancy] = []
 
+    def __init__(self):
+        self.update_from_site()
+        # self.read_from_file()
+
     def update_from_site(self):
-        """Метод получения информации и записи в список"""
+        """Метод получения информации с сайта и записи в список"""
         parser_sj = SJParser()
         for el in parser_sj.get_vacations():
-            self.list_sj.append(SuperJobVacancy(
+            new_vacancy = SuperJobVacancy(
                 title=el['title'],
                 salary_max=el['salary_max'],
                 salary_min=el['salary_min'],
                 employer=el['employer'],
                 link=el['link'],
                 source=el['source'],
-            ))
+            )
+            self.list_sj.append(new_vacancy)
+
         parser_hh = HHParser()
         for el in parser_hh.get_vacations():
-            self.list_hh.append(HeadHunterVacancy(
+            new_vacancy = HeadHunterVacancy(
                 title=el['title'],
                 salary_max=el['salary_max'],
                 salary_min=el['salary_min'],
                 employer=el['employer'],
                 link=el['link'],
                 source=el['source'],
-            ))
+            )
+            self.list_hh.append(new_vacancy)
 
     def read_from_file(self, filename: str = 'db.json'):
         """Метод для чтения файла с возможностью получения информации из вакансий"""
@@ -61,46 +68,31 @@ class Service:
     def write_to_file(self, filename: str = 'db.json'):
         """Метод записи в файл найденных вакансий"""
         with open(filename, 'w') as f:
-            data = self.list_sj + self.list_hh
+            data = self.list_sj + self.list_hh  # noqa
             data_dict = []
             for el in data:
                 data_dict.append(el.to_dict())
             f.write(json.dumps(data_dict, indent=4, ensure_ascii=False))
 
-    # def find_by_name(self, vacation_name: str):
-    #     """Метод поиска данных по названию, например: по названию профессии"""
-    #     with open('db.json', 'r') as f:
-    #         raw_data = f.read()
-    #         data = json.loads(raw_data)
-    #         research = []
-    #         for el in data:
-    #             if el['title'] in vacation_name:
-    #                 research.append(el)
-    #                 print(research)
+    def find_by_name(self, vacation_name: str):
+        """Метод поиска данных по названию, например: по названию профессии"""
+        with open('db.json', 'r') as f:
+            raw_data = f.read()
+            data = json.loads(raw_data)
+            for el in data:
+                if vacation_name in el['title']:
+                    print(el)
+            else:
+                print("У нас нету таких данных, обновите данные")
+
     #
-    # def find_by_salary(self, salary: int):
-    #     """Метод поиска данных по зарплате"""
-    #     with open('db.json', 'r') as f:
-    #         raw_data = f.read()
-    #         data = json.loads(raw_data)
-    #         research = []
-    #         for el in data:
-    #             if el['salary_min'] <= salary:
-    #                 research.append(el)
-    #         print(research)
-
-
-s = Service()
-# s.read_from_file()
-# s.find_by_salary(100000)
-# for el in s.list_sj:
-    # print(el.title)
-s.update_from_site()
-# for el in s.list_sj:
-#     print(el)
-s.write_to_file()
-# ss = SuperJobVacancy()
-
-
-
-
+    def find_by_salary(self, salary: int):
+        """Метод поиска данных по зарплате"""
+        with open('db.json', 'r') as f:
+            raw_data = f.read()
+            data = json.loads(raw_data)
+            research = []
+            for el in data:
+                if el['salary_min'] >= salary:
+                    research.append(el)
+            print(research)
